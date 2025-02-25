b168fec8f56dfe3d199ce909e7ef116707a21fe3
Alex Imbrea <alex@kappaml.com>
2025-02-26 00:38:03 +0100

    Export MetaForecaster in meta module

--- src/kappaml_core/meta/__init__.py
+++ src/kappaml_core/meta/__init__.py
@@ -2,8 +2,10 @@
 The :mod:`kappaml_core.meta` module contains meta-learning algorithms
 """
 
+from .meta_forecaster import MetaForecaster
 from .meta_regressor import MetaRegressor
 
 __all__ = [
+    "MetaForecaster",
     "MetaRegressor",
 ]
